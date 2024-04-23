22d384c8706286af6d75b02e578d53140c616afd
Marvel Ravindra Dioputra <ravindramarvel@gmail.com>
2024-04-23 20:01:41 +0700

    cart overlay

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -4,7 +4,6 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
   <title>HEJOTEKNO</title>
   <link href='https://fonts.googleapis.com/css?family=Roboto:400,300,700,500' rel='stylesheet' type='text/css'>
   <link rel="stylesheet" href="fonts/font-awesome/css/font-awesome.min.css"> <!-- Font Awesome -->
@@ -12,40 +11,94 @@
   <link rel="stylesheet" href="css/bootstrap.min.css"> <!-- Bootstrap Grid -->
   <link rel="stylesheet" href="https://cdn.linearicons.com/free/1.0.0/icon-font.min.css">
   <link rel="stylesheet" href="css/animate.min.css"><!-- Animate -->
-	<link rel="stylesheet" href="css/style.css"> <!-- Resource style -->
+  <link rel="stylesheet" href="css/style.css"> <!-- Resource style -->
   <link rel="stylesheet" href="css/magnific-popup.css"> <!-- Resource style -->
-
   <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
   <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
   <!--[if lt IE 9]>
     <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
   <![endif]-->
+  <!-- overlay cart -->
+  <style>
+    .overlay {
+      position: fixed;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      background-color: rgba(0, 0, 0, 0.5); /* warna hitam dengan opacity 0.5 */
+      z-index: 9998; /* letakkan di bawah cart-overlay */
+      display: none; /* sembunyikan secara default */
+    }
+    #cart-overlay {
+      position: fixed;
+      top: 0;
+      right: -300px; /* Start off-screen */
+      width: 300px;
+      height: 100%;
+      background-color: rgba(255, 255, 255, 1);
+      z-index: 9999;
+      transition: right 0.3s ease; /* Add transition for smooth sliding */
+    }
+
+    #cart-content {
+      position: absolute;
+      top: 50%;
+      right: 0;
+      transform: translateY(-50%);
+      width: 100%;
+      padding: 20px;
+      background-color: #fff;
+      color: #333;
+    }
+
+    #cart-content h3 {
+      margin-top: 0;
+    }
+
+    #checkout-btn {
+      display: block;
+      margin-top: 20px;
+      padding: 10px 20px;
+      background-color: #333;
+      color: #fff;
+      text-align: center;
+      text-decoration: none;
+    }
+
+    #checkout-btn:hover {
+      background-color: #555;
+    }
+  </style>
+
 </head>
 <body>
+  <div id="cart-overlay">
+    <div id="cart-content">
+      <h3>Your Cart</h3>
+      <div class="cart-items">
+        <!-- Isi dengan konten keranjang belanja -->
+      </div>
+      <a href="#" id="checkout-btn">Proceed to Checkout</a>
+    </div>
+  </div>
+  <div class="overlay"></div>
 
   <header id="main-nav">
     <div class="container">
-
       <a id="navigation" href="#"><i class="fa fa-bars"></i></a>
-
       <div id="slide_out_menu">
         <a href="#" class="menu-close"><i class="fa fa-times"></i></a>
-        <!-- <div class="logo"><img src="images/logohj-white.png" alt=""></div> -->
         <ul>
           <li><a href="/about">About</a></li>
           <li><a href="#">Products</a></li>
           <li><a href="#">Kamisama</a></li>
-          <li><a href="#">Help</a></li>
-          <li><a href="/contact">Contacts</a></li>
-          <li><a href="#" class="btn btn-green">Log In</a></li>
+          <li><a href="/contact">Contact Us</a></li>
+          <li><a href="#"><i class="fa fa-shopping-bag"></i></a></li>
         </ul>
-
         <div class="slide_out_menu_footer">
-          <div class="more-info">
-            <!-- <p>Made with love by <a href="http://getcraftwork.com">Craft Work</a></p>
-            <p>Developed by <a href="http://ruibogasdesign.net/">Rui Bogas</a> -->
-          </div>
+          <div class="more-info"></div>
           <ul class="socials">
             <li><a href="#"><i class="fa fa-twitter"></i></a></li>
             <li><a href="#"><i class="fa fa-facebook"></i></a></li>
@@ -56,7 +109,6 @@
           </ul>
         </div>
       </div>
-
       <div class="row">
         <div class="col-md-4">
           <ul class="left">
@@ -70,14 +122,13 @@
         </div>
         <div class="col-md-4">
           <ul class="right">
-              <li><a href="#" class="help">Help</a></li>
-              <li><a href="/contact">Contacts</a></li>
-              <li><a href="#" class="btn btn-green">Log In</a></li>
+            <li><a href="/contact">Contact Us</a></li>
+            <li><a href="#"><i class="fa fa-shopping-bag"></i></a></li>
           </ul>
         </div>
       </div>
     </div>
-  </header><!-- //Main Nav -->
+  </header>
 
   <section id="header">
     <div class="container">
@@ -141,9 +192,6 @@
             Projek TTI Hejotekno - ProKon
           </div>
         </div>
-        <div class="col-md-6 text-right">
-          <!-- <div class="made-by">Made with Love by <a href="http://getcraftwork.com" target="_blank"><img src="images/craftwork.png" alt=""></a></div> -->
-        </div>
       </div>
     </div>
   </footer>
@@ -153,5 +201,27 @@
   <script src="js/wow.min.js"></script>  <!-- wow -->
   <script src="js/jquery.magnific-popup.min.js"></script>  <!-- wow -->
   <script src="js/main.js"></script>  <!-- Main Script -->
+  <!-- script buat overlay cart -->
+  <script>
+   $(document).ready(function() {
+  $('.fa-shopping-bag').click(function(e) {
+    e.preventDefault();
+    $('#cart-overlay, .overlay').css('right', '0'); // Slide in from the right
+    $('.overlay').fadeIn(); // tampilkan overlay
+  });
+
+  $('.overlay').click(function(e) {
+    if (e.target === this) {
+      $('#cart-overlay, .overlay').css('right', '-300px');
+      $('.overlay').fadeOut(); // sembunyikan overlay
+    }
+  });
+
+  $('#checkout-btn').click(function(e) {
+    window.location.href = 'checkout.html';
+  });
+});
+
+  </script>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,10 @@ Route::get('/about', function(){
     return view('about');
 });
 
+Route::get('/products', function(){
+    return view('products');
+});
+
 Route::get('/contact', function(){
     return view('contact');
 });
