ecda35831dcca776975968f2afb0686846c5e843
daffahag123 <dfakhryanshori6@gmail.com>
2024-05-29 09:24:20 +0700

    tambah controller dan route halaman program

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\AboutController;
 use App\Http\Controllers\ContactController;
+use App\Http\Controllers\ProgramController;
 use App\Http\Controllers\ProductsController;
 
 Route::get('/dashboard/table', function(){
@@ -18,4 +19,5 @@ Route::get('/', [HomeController::class, 'index']);
 Route::get('/about', [AboutController::class, 'index']);
 Route::get('/products', [ProductsController::class, 'index']);
 Route::get('/productDetail', [ProductsController::class, 'detail']);
+Route::get('/program', [ProgramController::class, 'index']);
 Route::get('/contact', [ContactController::class, 'index']);
