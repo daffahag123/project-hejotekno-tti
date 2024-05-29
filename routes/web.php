c560b8698956a41f86a04c57bacae7b0c7860514
Faridz Fathin <faridz.fathin.mff2357@gmail.com>
2024-05-29 12:55:10 +0700

    web.php

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\AboutController;
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\ProgramController;
 use App\Http\Controllers\ProductsController;
-<<<<<<< HEAD
+
 use App\Http\Controllers\AdminController;
 
 use Illuminate\Support\Facades\Auth;
@@ -14,7 +14,6 @@ use Illuminate\Support\Facades\Auth;
 
 
 // =======
-=======
 
 Route::get('/dashboard/table', function(){
     return view('admin.dashboard');
@@ -24,12 +23,11 @@ Route::get('/dashboard/typography', function(){
     return view('admin.typography');
 });
 
->>>>>>> 75770b574d53bb8df90c3f9408740bf7dc0c00f1
 Route::get('/', [HomeController::class, 'index']);
 Route::get('/about', [AboutController::class, 'index']);
 Route::get('/products', [ProductsController::class, 'index']);
 Route::get('/productDetail', [ProductsController::class, 'detail']);
-<<<<<<< HEAD
+
 Route::get('/dashboard/table', [AdminController::class, 'index']);
 Route::get('/dashboard/typography', [AdminController::class, 'typo']);
 
@@ -40,10 +38,10 @@ Route::get('/login', function(){
 });
 
 //Auth::routes(['login' => false, 'register' => false]);
-=======
+
 Route::get('/program', [ProgramController::class, 'index']);
 Route::get('/contact', [ContactController::class, 'index']);
 Route::get('/checkout', function(){
     return view('checkout');
 });
->>>>>>> 75770b574d53bb8df90c3f9408740bf7dc0c00f1
+
