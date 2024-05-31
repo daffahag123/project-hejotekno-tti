75030c179189caae14d1537d0803214056a13260
Marvel Ravindra Dioputra <ravindramarvel@gmail.com>
2024-05-31 14:47:32 +0700

    footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,24 +1,26 @@
 <footer id="footer">
+  
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <div class="container footer-container">
       <div class="row">
         <div class="col-md-3">
           <img class="logo" src="images/HTHITAM.png" alt="" width="175">
           <p>PT. TOP TEKNO INDO</p>
           <ul class="socials">
-            <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-            <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-            <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+            <li><a href="https://www.instagram.com/hejotekno.indonesia/"><i class="fa fa-instagram"></i></a></li>
+            <li><a href="https://www.youtube.com/channel/UCiPGPJWy0f7eB4tuV0lCVsg/videos"><i class="fa fa-youtube-play"></i></a></li>
+            <!-- <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
             <li><a href="#"><i class="fa fa-tumblr"></i></a></li>
             <li><a href="#"><i class="fa fa-pinterest-p"></i></a></li>
-            <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+            <li><a href="#"><i class="fa fa-linkedin"></i></a></li> -->
           </ul>
         </div>
         <div class="col-md-2 col-md-offset-3 col-sm-4 col-xs-6 footer-links">
           <ul>
             <li><p class="title">Learn More</p></li>
-            <li><a href="#">StungtaXPindad</a></li>
-            <li><a href="#">Hetric</a></li>
-            <li><a href="#">Kamisama</a></li>
+            <li><a href="/product">StungtaXPindad</a></li>
+            <li><a href="/product">Hetric</a></li>
+            <li><a href="/program">Kamisama</a></li>
           </ul>
         </div>
         <div class="col-md-2 col-sm-4 col-xs-6 footer-links">
